4a1077f1e357bcfcc8c53c90b944464843e777be
Cliff Pyles <cliff@cliffpyles.com>
2024-02-21 09:49:08 -0500

    Add CodetoolsQuiz service

---
+++ services/codetools-quiz/config.py
@@ -0,0 +1,76 @@
+TOPICS = [
+    "Artificial Intelligence",
+    "Automated Testing",
+    "AWS",
+    "Cloud Platforms",
+    "Cloud Security",
+    "Computer Science",
+    "Continuous Delivery",
+    "Continuous Deployment",
+    "Continuous Integration",
+    "Cybersecurity",
+    "Data Analysis",
+    "Data Engineering",
+    "Data Science",
+    "DevOps",
+    "Enterprise Architecture",
+    "Git",
+    "Infrastructure as Code",
+    "JavaScript",
+    "Machine Learning",
+    "Microservices",
+    "Network Security",
+    "Penetration Testing",
+    "Platform Engineering",
+    "Python",
+    "React",
+    "Site Reliability Engineering",
+    "Software Architecture",
+    "Software Engineering",
+    "Solutions Architecture",
+    "Web Development",
+]
+
+
+TOPIC_GROUPS = {
+    "Software Development": [
+        "Computer Science",
+        "Software Engineering",
+        "JavaScript",
+        "Python",
+        "React",
+        "Git",
+    ],
+    "Software Architecture": [
+        "Software Architecture",
+        "Microservices",
+        "Enterprise Architecture",
+    ],
+    "DevOps": [
+        "DevOps",
+        "Continuous Integration",
+        "Continuous Delivery",
+        "Continuous Deployment",
+        "Site Reliability Engineering",
+        "Platform Engineering",
+        "Infrastructure as Code",
+    ],
+    "Security": [
+        "Penetration Testing",
+        "Network Security",
+        "Cloud Security",
+        "Cybersecurity",
+    ],
+    "Data Science & Engineering": [
+        "Data Science",
+        "Data Engineering",
+        "Machine Learning",
+        "Artificial Intelligence",
+        "Data Analysis",
+    ],
+    "Cloud & Infrastructure": ["AWS", "Cloud Platforms"],
+    "Quality Assurance & Testing": ["Automated Testing"],
+}
+
+
+SKILL_LEVELS = ["beginner", "intermediate", "advanced", "expert", "master"]
